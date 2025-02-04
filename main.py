6a2a48333bdb8e6ea283c60861b66a88be18fdd3
Taimafa <taima.fakheraldeen@live.biu.ac.il>
2025-02-04 13:26:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+from image_utils import load_image, edge_detection
+from skimage.filters import median
+from skimage.morphology import ball
+from PIL import Image
+lena=load_image('lena.jpg')
+clean=median(lena, ball(3))
+binary=l_edge > 100
+edge_image = Image.fromarray(np.uint8(binary * 255))
+edge_image.save('my_edges.png')
